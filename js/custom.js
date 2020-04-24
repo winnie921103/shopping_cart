1cc756fc56056feb1f7bffb223366aaa4a7c415b
winnie921103 <47648441+winnie921103@users.noreply.github.com>
2020-04-24 20:32:09 +0800

    done

--- js/custom.js
+++ js/custom.js
@@ -69,7 +69,25 @@
             let num = $(this).attr('data-target');
             confirmDelet(num);
         })
+        //回到頂端按鈕
 
+        $(function(){
+            // 點下按鈕之後，會以animate動態效果讓捲軸捲到網頁最頂端
+            $("#btn_goTop").click(function(){
+                jQuery("html,body").animate({
+                    scrollTop:0
+                },500);
+            });
+
+            // 目前的位置距離網頁頂端，大於200px就顯示按鈕，小於就隱藏
+            $(window).scroll(function() {
+                if ( $(this).scrollTop() > 200){
+                    $('#btn_goTop').fadeIn("fast");
+                } else {
+                    $('#btn_goTop').stop().fadeOut("fast");
+                }
+            });
+        });
 
     });
 })(this.jQuery);
